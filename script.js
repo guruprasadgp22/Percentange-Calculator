ac61ceaa5d2eb301171f2729eb219f992c888006
gp <guruprasad26gp@gmail.com>
2025-01-15 18:46:54 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const percentageValue = document.getElementById("percentageValue");
+
+    percentageSlider.addEventListener("input", function(){
+        percentageValue.textContent = `${percentageSlider.value}%`;
+    });
+});
+
+function calculatePercentage() {
+    const percentageSlider = document.getElementById("percentageSlider").value;
+    const baseNumber = document.getElementById("baseNumber").value;
+
+    if(baseNumber === ""){
+        alert("Please enter a base number");
+        return
+    }
+
+    const result = (parseFloat(baseNumber) * parseFloat(percentageSlider) / 100).toFixed(2);
+    document.getElementById("result").innerHTML = `Result : ${result}`;
+}
